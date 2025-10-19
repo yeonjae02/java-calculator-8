9460fc019a3da5f6399dc804abcaa48e89f15ed4
yeonjae02 <0yeonjae2@naver.com>
2025-10-19 10:34:06 +0900

    feat(Application) : 프로그램 실행 로직 구현
    - input, calculator, output을 연결하여 프로그램을 실행할 수 있도록 하였습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import calculator.domain.StringCalculator;
+import calculator.view.CalculatorInput;
+import calculator.view.CalculatorOutput;
+
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculator calculator = new StringCalculator();
+        String exp = CalculatorInput.getUserInput();
+        long result = calculator.addNumbers(exp);
+        CalculatorOutput.printAnswer(result);
     }
+
 }
+
